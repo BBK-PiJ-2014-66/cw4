a1339a513d920add8370d661e2365a2dffe8b1bd
Oliver Smart <osmart01@dcs.bbk.ac.uk>
2015-02-27 13:21:31 +0000

    Simplify approach by adding method ... registerExistingID(int) so this can be used after contacts read from file.

--- src/main/IdContact.java
+++ src/main/IdContact.java
@@ -1,8 +1,5 @@
-import java.io.ObjectStreamException;
-import java.io.Serializable;
-
 /**
- * Singleton class to ensure that unique contactID's are kept
+ * Singleton class used to issue unique ID numbers for contacts.
  * 
  * Written after looking at information from:
  * http://www.javalobby.org/java/forums/t17491.html
@@ -11,7 +8,7 @@ import java.io.Serializable;
  * http://www.tutorialspoint.com/design_pattern/singleton_pattern.htm
  *
  */
-class IdContact implements Serializable {
+class IdContact {
 	private static final IdContact INSTANCE = new IdContact();
 	private int maxID = 0; // the maximum ID seen so far
 
@@ -28,21 +25,24 @@ class IdContact implements Serializable {
 	}
 
 	/**
-	 * provides a unique integer ID for contacts
+	 * provides a unique integer ID for contacts.
 	 * 
-	 * @return the ID number
+	 * @return the new ID number that is one larger than any ID issued so far
 	 */
 	public int nextID() {
 		return ++maxID;
 	}
 
-	private Object readResolve() throws ObjectStreamException {
-		// instead of the object we're on,
-		// return the class variable INSTANCE
-		return INSTANCE;
+	/**
+	 * Used to register contacts that have been read in from a file to make sure
+	 * no new ID issued conflicts with it.
+	 * 
+	 * @param existingID
+	 *            the existing ID read in from file.
+	 */
+	public void registerExistingID(int existingID) {
+		if (existingID > maxID)
+			maxID = existingID;
 	}
-}
 
-/*
- * id = IdContact.getInstance.nextID();
- */
+}
