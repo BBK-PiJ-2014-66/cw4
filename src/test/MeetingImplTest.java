0b491628dae54232e5b555c48a611056e591c5be
Oliver Smart <osmart01@dcs.bbk.ac.uk>
2015-02-28 04:03:05 +0000

    JUnit tests for MeetingImpl. N.B. Not yet compiled as class not yet written.

---
+++ src/test/MeetingImplTest.java
@@ -0,0 +1,53 @@
+import java.util.Calendar;
+import java.util.GregorianCalendar;
+import java.util.HashSet;
+import java.util.Set;
+
+import org.junit.Test;
+import static org.junit.Assert.assertThat;
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.CoreMatchers.not;
+
+/**
+ * JUnit tests for MeetingImpl. A pretty simple class single constructor, 3
+ * getters.
+ * 
+ * @author Oliver Smart {@literal <osmart01@dcs.bbk.ac.uk>}
+ * @since 28 February 2015
+ *
+ */
+public class MeetingImplTest {
+
+	/**
+	 * Use constructor to produce a simple Meeting object and check we can
+	 * get back the supplied data correctly.
+	 */
+	@Test
+	public void testConstructAndGetters() {
+		Contact testContact = new ContactImpl("Test User");
+		Set<Contact> testContacts = new HashSet<>();
+		testContacts.add(testContact);
+		Calendar testDate = new GregorianCalendar(2015, 0, 01); // 1st January
+		Meeting testMeeting = new MeetingImpl(testContacts, testDate);
+		assertThat("Get back the same date as provided to constructor?",
+				testMeeting.getDate(), is(testDate));
+		assertThat("Get back the same contacts as provided to constructor?",
+				testMeeting.getContact(), is(testContacts));
+	}
+
+	/**
+	 * Create two Meeting objects and check that their ID's are distinct.
+	 */
+	@Test
+	public void testTwoMeetingsHaveDifferentIDs() {
+		Contact testContact = new ContactImpl("Test User");
+		Set<Contact> testContacts = new HashSet<>();
+		testContacts.add(testContact);
+		Calendar testDate = new GregorianCalendar(2015, 0, 01);
+		Meeting testMeetA = new MeetingImpl(testContacts, testDate);
+		Meeting testMeetB = new MeetingImpl(testContacts, testDate);
+		assertThat("Two meetings have distinct ID's", testMeetA.getId(),
+				not(testMeetB.getId()));
+	}
+
+}
