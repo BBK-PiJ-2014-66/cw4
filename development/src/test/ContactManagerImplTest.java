ed55e2892feefcddc54623eff39d880d3f35d325
Oliver Smart <osmart01@dcs.bbk.ac.uk>
2015-03-01 18:55:17 +0000

    addFutureMeeting() with an unknown ... contact now produces the specified exception, uses checkContacts() method.
    All current unit tests pass

--- development/src/test/ContactManagerImplTest.java
+++ development/src/test/ContactManagerImplTest.java
@@ -25,7 +25,7 @@ import cw4.FutureMeeting;
  * JUnit tests for ContactManagerImpl implementation of ContactManager. N.B.,
  * often use ContactManagerPlus interface to use simple getters.
  * 
- * All tests ***************
+ * All tests now pass.
  * 
  * @author Oliver Smart {@literal <osmart01@dcs.bbk.ac.uk>}
  * @since 24 February 2015
