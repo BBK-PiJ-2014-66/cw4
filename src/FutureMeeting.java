75757dc038cdccd583ea314e1d478e810aebbe42
Oliver Smart <osmart01@dcs.bbk.ac.uk>
2015-03-10 18:37:49 +0000

    delete src directory and the interfaces from development

--- src/FutureMeeting.java
+++
@@ -1,9 +0,0 @@
-/**
- * A meeting to be held in the future
- * 
- * @author "PiJ code given on cw4.pdf"
- */
-public interface FutureMeeting extends Meeting {
-	// No methods here, this is just a naming interface
-	// (i.e. only necessary for type checking and/or downcasting)
-}
