8a42a94912e084ea55ddc972d6afd7f28624ab8e
Oliver Smart <osmart01@dcs.bbk.ac.uk>
2015-03-12 03:56:51 +0000

    SerialEncoder is abstract class rather .. than interface. Had rethink see https://github.com/BBK-PiJ-2014-66/cw4/wiki/Refactoring-to-separate-FileIO-and-Serialization

--- src/serialencoder/SerialEncoder.java
+++ src/serialencoder/SerialEncoder.java
@@ -9,21 +9,21 @@ package serialencoder;
  * @since 11 March 2015
  * 
  */
-public interface SerialEncoder {
+public abstract class SerialEncoder {
 	/**
 	 * Converts the supplied obj to a string so that it can be subsequently
-	 * recovered by {@link #???}.
+	 * recovered by {@link #decode(String str)}.
 	 * 
 	 * @param obj
-	 *            the object to encode
+	 *            the object to encode (may need to be Serializable)
 	 * @return encoded form of the object
 	 * @throws RuntimeException
 	 *             if there is any problem encoding the object
 	 */
-	public String encode(Object obj);
+	public abstract String encode(Object obj);
 
 	/**
-	 * Decodes a string created by {@link #encode(Object obj} recovering the
+	 * Decodes a string created by {@link #encode(Object obj)} recovering the
 	 * Object.
 	 * 
 	 * @param str
@@ -32,5 +32,30 @@ public interface SerialEncoder {
 	 * @throws RuntimeException
 	 *             if there is any problem decoding the string
 	 */
-	public Object decode(String str);
+	public abstract Object decode(String str);
+
+	/**
+	 * saves the object to a file, so that it can be read at a later time by
+	 * {@link retreiveFromFile}
+	 * 
+	 * @param obj the Object to be saved (may need to be Serializable)
+	 * @param outFileName the name for the output file
+	 * @throws RuntimeException
+	 *             if there is any problem opening the file or encoding the
+	 *             object
+	 */
+	public void saveToFile(Object obj, String outFileName) {
+		throw new RuntimeException("not yet implemented"); // TODO
+	}
+
+	/**
+	 * retrieves an object from a file created by {@link saveToFile}
+	 * 
+	 * @param inFileName the name for the input file (must exist)
+	 * @return the Object encoded in the file
+	 */
+	public Object retreiveFromFile(String inFileName) {
+		throw new RuntimeException("not yet implemented"); // TODO
+	}
+
 }
