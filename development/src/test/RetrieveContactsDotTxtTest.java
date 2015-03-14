ddead719a6c29f714af6689de08c88f6d992d8a5
Oliver Smart <osmart01@dcs.bbk.ac.uk>
2015-03-14 09:53:20 +0000

    Take out unused imports!

--- development/src/test/RetrieveContactsDotTxtTest.java
+++ development/src/test/RetrieveContactsDotTxtTest.java
@@ -1,35 +1,15 @@
 package test;
 
-import static org.junit.Assert.assertNull;
-import static org.junit.Assert.assertThat;
-import static org.junit.Assert.assertNotNull;
-import static org.junit.Assert.assertTrue;
-import static org.junit.Assert.fail;
 import static org.hamcrest.CoreMatchers.is;
-import static org.hamcrest.CoreMatchers.not;
+import static org.junit.Assert.assertThat;
 
-import java.io.File;
-import java.io.IOException;
-import java.nio.file.Files;
-import java.util.Arrays;
 import java.util.Calendar;
-import java.util.Date;
 import java.util.GregorianCalendar;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
 
-import org.junit.Before;
 import org.junit.Test;
 
-import cw4.Contact;
-import cw4.ContactImpl;
-import cw4.ContactManager;
 import cw4.ContactManagerImpl;
 import cw4.ContactManagerPlus;
-import cw4.FutureMeeting;
-import cw4.Meeting;
-import cw4.PastMeeting;
 
 /**
  * Single test run in isolation:
