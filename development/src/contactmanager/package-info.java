0f4462838fb621d1e230e1470600269315cc5c84
Oliver Smart <osmart01@dcs.bbk.ac.uk>
2015-03-14 11:53:01 +0000

    JavaDoc review. Add package descriptions

---
+++ development/src/contactmanager/package-info.java
@@ -0,0 +1,7 @@
+/**
+ * Provides the classes to create a simple contact manager.
+ * <p>
+ * A contact manager keeps track of Contacts (who will be at a meeting) and
+ * Meetings both future and past.
+ */
+package contactmanager;
---
+++ src/serialencoder/package-info.java
@@ -0,0 +1,5 @@
+/**
+ * Provides methods to encode an Object to a String or to a file and to decode it back using
+ * serialization.
+ */
+package serialencoder;
