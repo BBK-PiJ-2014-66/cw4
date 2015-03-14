00c407c6e50d1c257c8d13cb71ad8a8f30fb122f
Oliver Smart <osmart01@dcs.bbk.ac.uk>
2015-03-14 13:17:15 +0000

    Add author and date information

--- src/contactmanager/package-info.java
+++ src/contactmanager/package-info.java
@@ -3,5 +3,8 @@
  * <p>
  * A contact manager keeps track of Contacts (who will be at a meeting) and
  * Meetings both future and past.
+ * 
+ * @author Oliver Smart {@literal <osmart01@dcs.bbk.ac.uk>}
+ * @since 24 February 2015
  */
 package contactmanager;
--- src/serialencoder/package-info.java
+++ src/serialencoder/package-info.java
@@ -1,5 +1,9 @@
 /**
  * Provides methods to encode an Object to a String or to a file and to decode it back using
  * serialization.
+ * 
+ * @author Oliver Smart {@literal <osmart01@dcs.bbk.ac.uk>}
+ * @since 11 March 2015
+ *
  */
 package serialencoder;
