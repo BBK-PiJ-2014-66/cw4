754ec03d6774def418215efd763f0feb96e4a98d
Oliver Smart <osmart01@dcs.bbk.ac.uk>
2015-03-20 11:10:08 +0000

    Refactoring #2 clean up time. Create timetools and NowOrPretend singleton.
    see
    https://github.com/BBK-PiJ-2014-66/cw4/wiki/Refactoring-%232-clean-up-time

---
+++ src/uk/fictitiousurl/timetools/CalendarUtils.java
@@ -0,0 +1,41 @@
+package uk.fictitiousurl.timetools;
+
+import java.util.Calendar;
+
+/**
+ * Class to Calendar related utilities
+ * 
+ * @author Oliver Smart {@literal <osmart01@dcs.bbk.ac.uk>}
+ * @since 02 March 2015
+ * 
+ */
+class CalendarUtils {
+
+	private CalendarUtils() {
+		throw new UnsupportedOperationException("Uninstantiable class");
+	}
+
+	/**
+	 * Compare two Calendar times and returns true if they are on the same date
+	 * (day of month, month and year) ignoring time of day. Does not take time
+	 * zones into account.
+	 * 
+	 * @param calA
+	 *            the first calendar time
+	 * @param calB
+	 *            the second calendar time
+	 * @return true if the dates match, false otherwise
+	 */
+	static boolean sameDate(Calendar calA, Calendar calB) {
+		boolean match = true;
+		if (calA.get(Calendar.YEAR) != calB.get(Calendar.YEAR)) {
+			match = false;
+		} else if (calA.get(Calendar.MONTH) != calB.get(Calendar.MONTH)) {
+			match = false;
+		} else if (calA.get(Calendar.DATE) != calB.get(Calendar.DATE)) {
+			// (DATE is the day in the month)
+			match = false;
+		}
+		return match;
+	}
+}
---
+++ src/uk/fictitiousurl/timetools/NowOrPretend.java
@@ -0,0 +1,58 @@
+package uk.fictitiousurl.timetools;
+
+import java.util.Calendar;
+
+/**
+ * singleton class to provide either the real system time
+ * for now or a "pretend" time.
+ *
+ * @author Oliver Smart {@literal <osmart01@dcs.bbk.ac.uk>}
+ * 
+ */
+public enum NowOrPretend {
+	TIME; 
+
+	/**
+	 * Normally the real system date time will be used to check whether a
+	 * meeting is in the future or past. However if pretendNow is not null then
+	 * this date time will be used instead. Intended for testing.
+	 */
+	private Calendar pretendNow = null;
+
+	/**
+	 * Used to set a false current date in place of the real actual date/time
+	 * "now". Useful for testing and may be for the program.
+	 * 
+	 * @param pretendNow
+	 *            the date/time to treat as "now".
+	 */
+	public void setPretendNow(Calendar pretendNow) {
+		this.pretendNow = pretendNow;
+	}
+
+	/**
+	 * Getter for pretendNow - the overriding date for "now".
+	 * 
+	 * @return the date/time to treat as "now" or null if no pretend is set (so
+	 *         real system time will be used).
+	 */
+	public Calendar getPretendNow() {
+		return pretendNow;
+	}
+
+	/**
+	 * check whether a date is in the future N.B. checks against current
+	 * date/time unless {@link #overrideDateNow(Calendar) overrideDateNow} has
+	 * been used to set a "pretendNow"
+	 * 
+	 * @param date
+	 *            the date to check
+	 * @return whether the date is in the future.
+	 */
+	public boolean checkDateIsFuture(Calendar date) {
+		Calendar now = (pretendNow != null) ? pretendNow : Calendar
+				.getInstance();
+		return date.after(now);
+	}
+
+}
